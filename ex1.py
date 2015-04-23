08172a951d5ba7c93f6a27cfdcb439454b15cd82
Brian McEntee <brian.m.mcentee@gmail.com>
2015-04-23 01:12:24 -0400

    first example complete

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World"
+#print "I Like typing this"
+#print "This is fun!"
+#print "Yay I'm printing!"
+#print 'Yay! Printing'
+#print "I'd much rather you 'not'"
+#print 'I "said" do not touch this'
+#print 'new line'
