23dc7fe864f1d2e7de63eaf178626c004a20384c
Brian McEntee <brian.m.mcentee@gmail.com>
2015-04-25 20:13:16 -0400

    finished ex3.py and study drills

---
+++ ex3.py
@@ -0,0 +1,34 @@
+# begin counting chickens
+print "I will now count my count my chickens"
+
+#Hens is 25 + (30 / 6) = 30
+print "Hens", 25 + 30 / 6
+#Roosters is 100 - (25 * 3) % 4
+print "Roosters", 100 - 25 * 3 % 4
+
+#begin counting eggs
+print "Now I will count my eggs"
+#eggs is 3 + 2 + 1 - 5 + (4 % 2) - (1/4) + 6 
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Test boolean logic
+print "Is is true that 3 + 2 < 5 - 7?"
+#prints False
+print 3 + 2 < 5 - 7
+
+#Asks what is 3 + 2 and then prints the answer
+print "What is 3 + 2? ", 3 + 2
+#Asks what is 5 - 7 and then prints the answer
+print "What is 5 - 7?", 5  - 7
+
+#you moron
+print "Oh, thats why its False"
+
+print "How about some more"
+
+#prints True
+print "Is is greater?", 5 > -2
+#prints True
+print "Is is greater than or equal to?", 5 >= -2
+#prints False
+print "Is is less than equal to?", 5 <= -2
