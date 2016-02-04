2a299e2d95e7c0ca8e51cf23df7dd13c25f467af
Brian McEntee <brian.m.mcentee@gmail.com>
2016-02-03 20:12:25 -0500

    Completed up to exercise 12. Now beginning to work with user input.

---
+++ ex11_2.py
@@ -0,0 +1,7 @@
+print "What is your mood today?"
+mood = raw_input("current mood: ")
+
+print "What day of week is it?"
+day = raw_input("current day: ")
+
+print "So it is currently a %r and your mood is %r" % (day, mood)
