1a6c5efd17ccf8b368950bcb1c9d7321037da98f
Brian McEntee <brian.m.mcentee@gmail.com>
2016-02-10 18:25:27 -0500

    Completed exercise 34, indexing items in a list.

---
+++ ex34.py
@@ -0,0 +1,13 @@
+# Learning to index elements contained in a list. Just like arrays in Java!
+
+animals = ["bear", "tiger", "penguin", "zebra"]
+bear = animals[0]
+
+print bear
+
+nums = range(1,11) # list of ints 1 to 10 
+
+my_num = nums[9] # The tenth element ie 10
+his_num = nums[0] # The first element ie 1
+
+print my_num, his_num
