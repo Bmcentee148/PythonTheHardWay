57d97555b05047e041ebfa9fbbbef5e317fcbccb
Brian McEntee <brian.m.mcentee@gmail.com>
2016-02-10 23:28:39 -0500

    Starting a game of my own for practice named 'ex36.py'. File currently has header comments but no other code ie clean slate.

---
+++ ex36.py
@@ -0,0 +1,8 @@
+# This is a game based of previous examples for practice. The game will 
+# give the user choices based on situations we give them and will
+# progress based on those decisions. The user will either end the game
+# successfully, or die along the way. The game will terminate in either case.
+# 
+# Brian McEntee 2.10.16
+# version 1
+
