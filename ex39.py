4f87b14f458db5612904199ab379ec910337dfed
Brian McEntee <brian.m.mcentee@gmail.com>
2016-03-10 00:47:06 -0500

    Finished ex39.py

--- ex39.py
+++ ex39.py
@@ -1,17 +1,17 @@
 # create a mapping of state to abbreviation
-states = [
+states = {
     'Oregon' : 'OR',
     'Florida' : 'FL',
     'California' : 'CA',
     'New York' : 'NY',
     'Michigan' : 'MI'
-]
+}
 
-cities = [
+cities = {
     'CA' : 'San Francisco',
     'MI' : 'Detroit',
     'FL' : 'Jacksonville'
-]
+}
 
 cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
@@ -26,3 +26,34 @@ print '-' * 10
 print "Michigan's abbreviation is: ", states['Michigan']
 print "Florida's abbreviation is: ", states['Florida']
 
+# do it using the cities and states dicts
+print '-' * 10
+print "Michigan State has: ", cities[states['Michigan']]
+print "Florida State has: ", cities[states['Florida']]
+
+# print every state abbreviation
+print '-' * 10
+for state, abbreviation in states.items() :
+    print "%s is abbreviated as %s." % (state, abbreviation)
+
+# print every city in state
+print '-' * 10
+for abbrev, city in cities.items() :
+    print "%s has the city %s" % (abbrev, city)
+
+# now do both at the same time
+print '-' * 10
+for state, abbreviation in states.items() :
+    "%s state is abbreviated as %s, and has the city %s" % (
+        state, abbreviation, cities[abbreviation])
+
+print '-' * 10
+# safely get an abbreviation given a state that might not be there
+state = states.get('Texas', None)
+
+if not state:
+    print "Sorry, no Texas."
+
+# get a city with a default value
+city = cities.get('TX', 'Does not exist')
+print "The city for the state Texas is: %s" % city
