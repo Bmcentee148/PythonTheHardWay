5924028572beff648e2f5ce23bc9d3c664c90cf9
Brian McEntee <brian.m.mcentee@gmail.com>
2016-03-14 16:48:08 -0400

    Add 'ex43.py'. Create the skeleton for the class structure.

---
+++ ex43.py
@@ -0,0 +1,23 @@
+class Map :
+    pass
+
+class Engine :
+    pass
+
+class Scene :
+    pass
+
+class Death(Scene) :
+    pass
+
+class Central_Corridor(Scene) :
+    pass
+
+class Armory(Scene) :
+    pass
+
+class Bridge(Scene) :
+    pass
+
+class Escape_Pod(Scene) :
+    pass
