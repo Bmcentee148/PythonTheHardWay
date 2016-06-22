06c0a1186aa8c105051c930642758c1a90a4c81d
Brian McEntee <brian.m.mcentee@gmail.com>
2016-06-21 22:14:20 -0400

    Complete exercise 50 by setting up a simple web application project 'gothonweb'.

--- gothonweb/bin/app.py
+++ gothonweb/bin/app.py
@@ -3,16 +3,25 @@ import web
 
 #-- Main Script --#
 
+# We can add multiple urls in a single tuple following the path, class pattern
 urls = (
-    '/' ,'index'
+    '/' ,'index',
+    '/hello', 'hello'
 )
 
 app = web.application(urls, globals())
 
+render = web.template.render('templates/')
+
 class index:
     def GET(self) :
-        greeting = 'Hello, world!'
-        return greeting
+        result = 'Brian'
+        return render.index(name = result) 
+
+class hello: 
+    def GET(self) :
+        greeting = 'Hello, World!'
+        return render.hello(greeting = greeting)
 
 if __name__ == '__main__' :
     app.run()
