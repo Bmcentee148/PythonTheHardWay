679f62b7e177cfabea90e6e7200f7f9e992b6308
Brian McEntee <brian.m.mcentee@gmail.com>
2016-07-01 02:12:49 -0400

    Play with setup for final project, gothonweb_v4 for exercise 52.

---
+++ gothonweb_v4/bin/app.py
@@ -0,0 +1,25 @@
+import web
+
+#map of urls to handler classes
+urls = (
+    '/hello', 'Index'
+)
+
+app = web.application(urls, globals())
+render = web.template.render('templates/', base='layout')
+
+# Handler class for the index page
+class Index(object) :
+
+    #Handler method for a GET request
+    def GET(self) :
+        return render.hello_form()
+
+    #Handler method for a POST request
+    def POST(self) :
+        form = web.input(greet = "Hello", name = "Nobody")
+        greeting = "{0} {1}".format(form.greet, form.name)
+        return render.index(greeting = greeting)
+
+if __name__ == '__main__' :
+    app.run()
